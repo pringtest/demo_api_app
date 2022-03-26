652a7aaeca4a13bf6eec1c81bb2aa2ec26a2b895
syamim <syamim_nsl@yahoo.com>
2022-03-26 21:48:26 +0800

    dynamodb api done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { getDynamoDB } = require("./src");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, function () {
+  console.log(`listening on ${port}`);
+});
+
+app.get("/dynamodb", async (req, res, next) => {
+  try {
+    var resp = await getDynamoDB.handler();
+    if (resp.statusCode == 200) {
+      console.log("resp", resp)
+      res.send(JSON.parse(resp.body));
+    } else {
+      throw resp
+    }
+  } catch (err) {
+    console.log("err", err)
+    res.status(err.statusCode).send(JSON.parse(err.body))
+  }
+});
+
+app.get("/rds", (req, res) => {
+  res.send("RDS");
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const getDynamoDB = require("./get_DynamoDB");
+const getRDS = require("./get_RDS");
+
+module.exports = {
+   getDynamoDB,
+   getRDS
+};
